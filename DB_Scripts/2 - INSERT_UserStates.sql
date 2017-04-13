da159fe61698b8ce089961fb69891f209b2835fe
Nico Pellegrinet <nicopellegrinet@hotmail.com>
2017-04-13 00:06:36 -0300

    DB Scripts.

---
+++ DB_Scripts/2 - INSERT_UserStates.sql
@@ -0,0 +1,8 @@
+USE [DotW]
+GO
+INSERT INTO [dbo].[UserStates] VALUES(1, 'Active')
+GO
+INSERT INTO [dbo].[UserStates] VALUES(2, 'Suspended')
+GO
+INSERT INTO [dbo].[UserStates] VALUES(3, 'Blocked')
+GO
