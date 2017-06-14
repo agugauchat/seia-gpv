992964eb54e4ae2f21fb21128e9a935b3b27d24a
Nico Pellegrinet <nicopellegrinet@hotmail.com>
2017-06-13 22:20:45 -0300

    Use application/json in responses.

--- DotW/Web.DotW.API/Startup.cs
+++ DotW/Web.DotW.API/Startup.cs
@@ -13,7 +13,9 @@
     using System.Collections.Generic;
     using System.Configuration;
     using System.Linq;
+    using System.Net.Http;
     using System.Net.Http.Formatting;
+    using System.Net.Http.Headers;
     using System.Web;
     using System.Web.Http;
 
@@ -85,6 +87,28 @@
 
             var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
             jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Services.Replace(typeof(IContentNegotiator), new JsonContentNegotiator(jsonFormatter));
+        }
+    }
+
+    internal class JsonContentNegotiator : IContentNegotiator
+    {
+        private readonly JsonMediaTypeFormatter _jsonFormatter;
+
+        public JsonContentNegotiator(JsonMediaTypeFormatter formatter)
+        {
+            _jsonFormatter = formatter;
+        }
+
+        public ContentNegotiationResult Negotiate(
+                Type type,
+                HttpRequestMessage request,
+                IEnumerable<MediaTypeFormatter> formatters)
+        {
+            return new ContentNegotiationResult(
+                _jsonFormatter,
+                new MediaTypeHeaderValue("application/json"));
         }
     }
 }
